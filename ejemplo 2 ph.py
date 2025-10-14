f2618648b4d8a6fe94b202f42ec53363b57b997e
Tu Nombre <analuczarate@gmail.com>
2025-10-14 10:04:04 -0300

    Primer commit

---
+++ ejemplo 2 ph.py
@@ -0,0 +1,5 @@
+#realizar un programa que calcule la superficie de un cuadrado 
+lado1= int (input("ingrese el lado 1:"))
+lado2=int (input("ingrese el valor 2:"))
+type(lado1)
+print("la superficie es: ",lado1*lado2)
---
+++ ejemplo 3 ph.py
@@ -0,0 +1,6 @@
+#calcular el sueldo de un empleado, tomando hs trabajadas y su valor por hora
+hstrabajadas=int (input("horastrabajadas"))
+valorHora=int(input("valorHora"))
+sueldo=int
+sueldo=hstrabajadas*valorHora
+print("El sueldo es:",sueldo)
